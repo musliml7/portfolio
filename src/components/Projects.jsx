a38762242d44f7e992987fabd52303512b193cad
ziyodullasodiqov2007 <ziyodullasodiqov2007@gmail.com>
2024-06-15 07:07:34 +0000

    v-0.3

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,51 +1,51 @@
 import React from 'react'
 
-    export default function Projects() {
-        return(
-            <div className="container">
-                <div className="row">
+export default function Projects() {
+    return (
+        <div className="container">
+            <div className="row">
+
                 <div className="col-4">
                     <div className="card">
-    <img src={'https://pmsolutionsja.com/wp-content/uploads/2018/04/project.jpg'} className="card-img-top" alt=""  style={{ width: '500px', height: '500px',  }}/>
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-    <div className="card-footer">
-        <small className="text-body-secondary">Last updated 3 mins ago</small>
-    </div>
-    </div>
-    </div>
-                <div className="col-4">
-                    <div classNameName="card-group">
-    
-    <div className="card">
-    <img src={'https://pmsolutionsja.com/wp-content/uploads/2018/04/project.jpg'} className="card-img-top" alt="" style={{ width: '200px', height: '200px',  }}/>
-    <div classNameName="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div className="card-footer">
-        <small className="text-body-secondary">Last updated 3 mins ago</small>
-    </div>
-    </div>
-    </div>
+                        <img src={'https://pmsolutionsja.com/wp-content/uploads/2018/04/project.jpg'} className="card-img-top" alt=""/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-body-secondary">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+                
                 <div className="col-4">
                     <div className="card">
-    <img src={'https://pmsolutionsja.com/wp-content/uploads/2018/04/project.jpg'} className="card-img-top" alt="" style={{ width: '500px', height: '500px',  }}/>
-    <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div className="card-footer">
-        <small className="text-body-secondary">Last updated 3 mins ago</small>
-    </div>
-    </div>
-    </div>
-            
-    
-</div>
-</div>
-</div>
-        )
-    }
+                        <img src={'https://pmsolutionsja.com/wp-content/uploads/2018/04/project.jpg'} className="card-img-top" alt=""/>
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-body-secondary">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-4">
+                    <div className="card">
+                        <img src={'https://pmsolutionsja.com/wp-content/uploads/2018/04/project.jpg'} className="card-img-top" alt="" />
+                        <div className="card-body">
+                            <h5 className="card-title">Card title</h5>
+                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-body-secondary">Last updated 3 mins ago</small>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
